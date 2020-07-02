04c22823136f3bce58a18c2ab5c623b42f27fa70
Jc Mandawe <jcpmandawe@gmail.com>
2020-07-02 17:10:57 +0800

    FEAT: Add createUser method

--- src/controllers/sample/sample.controller.ts
+++ src/controllers/sample/sample.controller.ts
@@ -1,18 +1,21 @@
-import { Get, Patch, Post, Delete, Put } from "@mayajs/common";
+import { Get, Patch, Post, Delete, Put, Check } from "@mayajs/common";
 import { Request, Response, NextFunction } from "express";
 import { Controller } from "@mayajs/core";
-import { SampleServices } from "./sample.service"
+import { SampleServices } from "./sample.service";
 
 @Controller({
   model: "./sample.model",
   route: "/sample",
 })
 export class SampleController {
-
   constructor(private services: SampleServices) {}
-  
-  @Get({ path: "/", middlewares: [] })
-  root(req: Request, res: Response, next: NextFunction): void {
-    res.send(this.services.hello());
+
+  @Post({
+    path: "/",
+    middlewares: [Check("firstName").isString().notEmpty().required(), Check("lastName").isString().notEmpty().required()],
+  })
+  async createUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const result = await this.services.create(req.body);
+    res.json(result);
   }
 }
