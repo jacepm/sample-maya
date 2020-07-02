681fa81ebe6806734ab73d1d6f8e2e1022bc67b5
Jc Mandawe <jcpmandawe@gmail.com>
2020-07-02 17:11:31 +0800

    FEAT: Add create in sample service

--- src/controllers/sample/sample.service.ts
+++ src/controllers/sample/sample.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from "@mayajs/core";
+import { Models } from "@mayajs/mongo";
 
 @Injectable()
 export class SampleServices {
+  @Models("sample") model: any;
 
   constructor() {}
-  
-  hello() {
-    return "Hello world!";
-  }
 
+  async create(body: any): Promise<any> {
+    let sample;
+
+    try {
+      sample = await this.model.create(body);
+    } catch (error) {
+      return { status: 422, message: error.errmsg ? error.errmsg : error.toString(), data: [], meta: {} };
+    }
+
+    try {
+      const result = await sample.save();
+      return { status: 200, message: "YES", data: result, meta: {} };
+    } catch (error) {
+      await this.model.findByIdAndDelete({ _id: sample._id });
+      return { status: 422, message: error.errmsg ? error.errmsg : error.toString(), data: [], meta: {} };
+    }
+  }
 }
