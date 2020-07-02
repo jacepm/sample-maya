bc269a23e6c1b73333742d84bd66df4f212ff2bd
Jc Mandawe <jcpmandawe@gmail.com>
2020-07-02 17:11:15 +0800

    FEAT: Add fields in sample model

--- src/controllers/sample/sample.model.ts
+++ src/controllers/sample/sample.model.ts
@@ -1,11 +1,27 @@
 import { MongoSchema, MongoModel } from "@mayajs/mongo";
+import paginate from "mongoose-paginate";
 
-const schema = MongoSchema({
-  name: {
-    required: [true, "Name is required."],
-    type: String,
-    unique: true,
+const options = {
+  timestamps: {
+    createdAt: "DateCreated",
+    updatedAt: "DateUpdated",
   },
-});
+};
+
+const schema = MongoSchema(
+  {
+    firstName: {
+      required: [true, "First Name is required."],
+      type: String,
+    },
+    lastName: {
+      required: [true, "Last Name is required."],
+      type: String,
+    },
+  },
+  options
+);
+
+schema.plugin(paginate);
 
 export default MongoModel("Sample", schema);
