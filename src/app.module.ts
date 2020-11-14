c5be13cff985805093180904edbc174a23100ea0
Jc Mandawe <jcpmandawe@gmail.com>
2020-11-13 20:17:41 -0800

    CHORE: Change database config to import Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { App } from "@mayajs/core";
-import { Mongo } from "@mayajs/mongo";
+import Mongo from "./databases/mongo";
 import { routes } from "./app.routing.module";
 
 @App({
   cors: true,
   logs: "dev",
-  port: 3333,
-  database: Mongo({
-    connectionString: "mongodb+srv://root:root@sample.u1uyv.mongodb.net/test?retryWrites=true&w=majority",
-    options: {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    },
-  }),
+  databases: [Mongo],
   routes,
 })
 export class AppModule {}
